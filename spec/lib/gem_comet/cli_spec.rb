dfef182f9f2f5ca99ad19517035a855e0a255675
ryz310 <ryz310@gmail.com>
2019-10-14 02:42:22 +0900

    Update specifications

--- spec/lib/gem_comet/cli_spec.rb
+++ spec/lib/gem_comet/cli_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe GemComet::CLI do
 
     it do
       cli.init
-      expect(cli).to have_received(:template)
+      expect(cli).to have_received(:template).twice
     end
   end
 
@@ -23,7 +23,7 @@ RSpec.describe GemComet::CLI do
 
       it 'executes commit and PR creation' do
         release!
-        expect(pr_comet).to have_received(:commit).twice
+        expect(pr_comet).to have_received(:commit).exactly(3).times
         expect(pr_comet).to have_received(:create!).twice
       end
     end
@@ -34,6 +34,22 @@ RSpec.describe GemComet::CLI do
       it do
         expect { release! }.to raise_error(/Verion number must be like a "1.2.3"/)
       end
+    end
+  end
+
+  describe '#changelog' do
+    let(:version) { '1.2.3' }
+    let(:version_editor) { instance_double(GemComet::VersionEditor, current_version: version) }
+
+    before do
+      allow(GemComet::VersionEditor).to receive(:new).and_return(version_editor)
+      allow(GemComet::ChangelogGenerator).to receive(:call)
+    end
+
+    it do
+      cli.changelog
+      expect(GemComet::ChangelogGenerator)
+        .to have_received(:call).with(current_version: version)
     end
   end
 
