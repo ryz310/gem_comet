113f0b2acbd7db76cc6acf086d8769c0da194196
ryz310 <ryz310@gmail.com>
2019-10-14 01:47:44 +0900

    Modify the location of the file path definition

--- lib/gem_comet/config.rb
+++ lib/gem_comet/config.rb
@@ -20,19 +20,13 @@ module GemComet
 
     private
 
-    attr_reader :file_path
-
-    def initialize(file_path: CONFIG_FILE_PATH)
-      @file_path = file_path
-    end
-
     def call
-      V1_1.from_hash(YAML.safe_load(File.open(file_path)))
+      V1_1.from_hash(YAML.safe_load(File.open(CONFIG_FILE_PATH)))
     rescue Errno::ENOENT
       raise 'Not initialized. Please run `$ gem_comet init`.'
     rescue TypeStruct::MultiTypeError => e
       puts e.message
-      raise "Unexpected file format. Please check your '#{file_path}'."
+      raise "Unexpected file format. Please check your '#{CONFIG_FILE_PATH}'."
     end
   end
 end
