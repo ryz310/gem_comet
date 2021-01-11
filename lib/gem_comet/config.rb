08e304a91fa2798e9ad9a7817d44fd171b9851ae
Ryosuke Sato <r-sato@feedforce.jp>
2021-01-11 19:48:06 +0900

    Add changelog categories option

--- lib/gem_comet/config.rb
+++ lib/gem_comet/config.rb
@@ -6,18 +6,23 @@ module GemComet
     using TypeStruct::Union::Ext
 
     CONFIG_FILE_PATH = '.gem_comet.yml'
-    CURRENT_VERSION = 1.1
+    CURRENT_VERSION = 1.2
 
     Release = TypeStruct.new(
-        base_branch: String,
-        release_branch: String,
-        version_file_path: String,
-        changelog_file_path: String | nil
-      )
+      base_branch: String,
+      release_branch: String,
+      version_file_path: String,
+      changelog_file_path: String | nil
+    )
 
-    V1_1 = TypeStruct.new(
+    Changelog = TypeStruct.new(
+      categories: TypeStruct::ArrayOf.new(String)
+    )
+
+    V1_2 = TypeStruct.new(
       version: 1.0...2.0,
-      release: Release
+      release: Release,
+      changelog: Changelog | nil
     )
 
     def initialize; end
@@ -25,7 +30,7 @@ module GemComet
     private
 
     def call
-      V1_1.from_hash(YAML.safe_load(File.open(CONFIG_FILE_PATH)))
+      V1_2.from_hash(YAML.safe_load(File.open(CONFIG_FILE_PATH)))
     rescue Errno::ENOENT
       raise 'Not initialized. Please run `$ gem_comet init`.'
     rescue TypeStruct::MultiTypeError => e
